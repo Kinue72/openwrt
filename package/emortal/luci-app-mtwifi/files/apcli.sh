e6987348bb75e84536e74783777967534fbc5b85
Kinue <kinue137@gmail.com>
2024-01-23 15:39:03 +0000

    Fixed apcli.sh script wrong interface
    Signed-off-by: Kinue <kinue137@gmail.com>

--- package/emortal/luci-app-mtwifi/files/apcli.sh
+++ package/emortal/luci-app-mtwifi/files/apcli.sh
@@ -1,23 +1,29 @@
 #!/bin/sh /etc/rc.common
 
-start() {
-	iwpriv apcli0 set ApCliEnable=0
-	ssid=$(grep -e "ApCliSsid=" /etc/wireless/mt7615/mt7615.1.dat)
-	enable=$(grep -e "ApCliEnable=" /etc/wireless/mt7615/mt7615.1.dat)
-	iwpriv apcli0 set "$ssid"
-	iwpriv apcli0 set ApCliAutoConnect=3
-    	iwpriv apcli0 set "$enable"
-    	iwpriv apclii0 set ApCliEnable=0
-	ssid=$(grep -e "ApCliSsid=" /etc/wireless/mt7615/mt7615.2.dat)
-	enable=$(grep -e "ApCliEnable=" /etc/wireless/mt7615/mt7615.2.dat)
-	iwpriv apclii0 set "$ssid"
-	iwpriv apclii0 set ApCliAutoConnect=3
-    	iwpriv apclii0 set "$enable"
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mt7615/mt7615.1.dat)
-    	iwpriv ra0 set "$kick"
-    	kick=$(grep -e "KickStaRssiLow=" /etc/wireless/mt7615/mt7615.2.dat)
-    	iwpriv rai0 set "$kick"
+apply_wireless_config() {
+  local interface=$1
+  local main_interface=$2
+  local dat_file=$3
+  local enable=$(grep -e "ApCliEnable=" "$dat_file")
+  
+  if [ "$enable" = "ApCliEnable=0" ]; then
+    iwpriv "$interface" set ApCliAutoConnect=0
+    iwpriv "$interface" set "$enable"
+    return
+  fi
+
+  local ssid=$(grep -e "ApCliSsid=" "$dat_file")
+  local kick=$(grep -e "KickStaRssiLow=" "$dat_file")
+
+  iwpriv "$interface" set "$ssid"
+  iwpriv "$interface" set ApCliAutoConnect=3
+  iwpriv "$interface" set "$enable"
+  iwpriv "$main_interface" set "$kick"
 }
 
-
-
+start() {
+  # 5GHz
+  apply_wireless_config "apcli0" "ra0" "/etc/wireless/mt7615/mt7615.1.5G.dat"
+  # 2.4GHz
+  apply_wireless_config "apclix0" "rax0" "/etc/wireless/mt7615/mt7615.1.2G.dat"
+}
